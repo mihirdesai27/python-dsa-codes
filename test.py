426b1f24bf5532bda1981c6d3417c6d27442b86f
Mihir Desai <mihir.999.desai@gmail.com>
2025-10-04 11:27:30 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+def max_min(arr):
+    smallest = largest = arr[0]
+    for num in arr:
+        if num < smallest:
+            smallest = num
+        if num > largest:
+            largest = num
+
+    return largest, smallest
+
+arr = [2,4,1,55,32,6,7,8,9]
+
+largest, smallest = max_min(arr)
+print("Smallest:", smallest)
+print("Largest:", largest)
+
+# Output should be:
+# Smallest: 1
+# Largest: 55
