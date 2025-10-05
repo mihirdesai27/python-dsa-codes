4253507c19952139562ed4fc5184fa9cb46b59db
Mihir Desai <mihir.999.desai@gmail.com>
2025-10-05 12:01:09 +0530

    rev

---
+++ revstring.py
@@ -0,0 +1,6 @@
+def rev_string(s):
+    """Return the reverse of the input string s."""
+    return s[::-1]  
+# Example usage:
+print(rev_string("hello"))  # Output: "olleh"
+print(rev_string("Python")) # Output: "nohtyP"
